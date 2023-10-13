617367fc31467a4a1124d29b2c506d299626b1b8
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-13 20:31:43 +0900

    initial commit

---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <h1>This is loading!</h1>;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>NOT FOUND YO!</h1>
+    </div>
+  );
+}
