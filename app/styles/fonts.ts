6143c507e1f847d17a7d95144f597ca33e87f25d
Boyeon Ihn <boyeonihn@gmail.com>
2023-10-16 18:36:04 +0900

    style: apply styling to pages

---
+++ app/styles/fonts.ts
@@ -0,0 +1,16 @@
+import { Grandiflora_One, Berkshire_Swash, Ultra } from 'next/font/google';
+
+export const grandi = Grandiflora_One({
+  weight: '400',
+  subsets: ['latin', 'latin-ext'],
+});
+
+export const ultra = Ultra({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+export const berkshire = Berkshire_Swash({
+  weight: '400',
+  subsets: ['latin', 'latin-ext'],
+});
